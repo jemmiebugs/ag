8712302c458d9e4bc4984d003249bb7a22e9c7ed
Jeminat Muritala <40564213+jemmiebugs@users.noreply.github.com>
2023-03-11 17:46:26 +0100

    Add files via upload

---
+++ site/templates/scripts/analyticsfc27.js
@@ -0,0 +1,13 @@
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+e.src='//www.google-analytics.com/analytics.js';
+r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+ga('create','UA-45029009-1','auto');ga('send','pageview');
+
+var $mcGoal = {'settings':{'uuid':'373a8671eb593e06291c1ba26','dc':'us9'}};
+(function() {
+	var sp = document.createElement('script'); sp.type = 'text/javascript'; sp.async = true; sp.defer = true;
+	sp.src = ('https:' == document.location.protocol ? 'https://s3.amazonaws.com/downloads.mailchimp.com' : 'http://downloads.mailchimp.com') + '/js/goal.min.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sp, s);
+})();
